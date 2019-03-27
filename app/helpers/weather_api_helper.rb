b692146dbc06341c037e1d5fe2d05e4c1af98cc2
Greg Garville <gjgarvil@gmail.com>
2019-03-27 15:39:23 -0400

    minor feature editing

--- app/helpers/weather_api_helper.rb
+++ app/helpers/weather_api_helper.rb
@@ -1,4 +1,8 @@
 require ‘oj’
 require ‘faraday’
 module WeatherApi
+    API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather?q=
+    '+city+','+ country +'&appid='+Api_Key
+
+    Api_Key = "2d43470cb5cd818066e33ed7180dc2a7"
 end
