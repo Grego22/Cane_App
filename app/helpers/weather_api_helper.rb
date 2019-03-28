0241d9aed5ea8145cefba6bc5bdd7510fdf2bf26
Greg Garville <gjgarvil@gmail.com>
2019-03-28 16:21:37 -0400

    added gem and controller, began forecast methods

--- app/helpers/weather_api_helper.rb
+++ app/helpers/weather_api_helper.rb
@@ -1,8 +1,19 @@
 require ‘oj’
 require ‘faraday’
 module WeatherApi
-    API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather?q=
-    '+city+','+ country +'&appid='+Api_Key
-
+    class WeatherApiCall
+        include faraday
+        base_uri = 'http://api.openweathermap.org/'
+        
+        def api_key
+            ENV['secret_api_key']
+        end
+        
+    
     Api_Key = "2d43470cb5cd818066e33ed7180dc2a7"
+    end
+
 end
+
+http://api.openweathermap.org/data/2.5/weather?q=
+    '+city+','+ country +'&appid='+Api_Key
